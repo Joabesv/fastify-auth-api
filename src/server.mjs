5b44e75d76587a696cfa40a715e981b071d9dc92
Joabesv <joabevarjao123@gmail.com>
2022-10-22 01:13:38 -0300

    start server and create stacks

---
+++ src/server.mjs
@@ -0,0 +1,32 @@
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const fastify = Fastify({ logger: true });
+const prisma = new PrismaClient();
+
+fastify.get('/stack', async (request, reply) => {
+  const stacks = await prisma.stack.findMany();
+  await reply.send(stacks);
+});
+
+fastify.post('/stack', async (request, reply) => {
+  const { tech, content } = request.body;
+  const stack = await prisma.stack.create({
+    data: {
+      tech,
+      content,
+    },
+  });
+  return reply.send(stack);
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 5000 });
+  } catch (err) {
+    console.log(`error on start ${err}`);
+    process.exit(1);
+  }
+};
+
+start();
