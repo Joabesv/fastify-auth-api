173bb1729a4e7e3337baaa2b5d54c91e7b64df41
Joabesv <joabevarjao123@gmail.com>
2022-11-02 01:29:40 -0300

    chore: change server to ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,16 @@
 import Fastify from 'fastify';
 import Autoload from '@fastify/autoload';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
 import { loggerSetup } from '../config/logger';
 
 const server = Fastify({ logger: loggerSetup });
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
+// eslint-disable-next-line
 server.register(Autoload, {
   dir: join(__dirname, '../plugins'),
 });
 
+// eslint-disable-next-line
 server.register(Autoload, {
   dir: join(__dirname, '../routes'),
 });
