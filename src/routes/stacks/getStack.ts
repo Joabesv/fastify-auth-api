51e1c9b6f6ecee76ee8701685395cccb4ed5dccb
Joabesv <joabevarjao123@gmail.com>
2022-11-03 00:38:58 -0300

    feat(getStack) - Typing getting one stack

--- src/routes/stacks/getStack.ts
+++ src/routes/stacks/getStack.ts
@@ -1,16 +1,24 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { prismaClient as prisma } from '../../database/connection';
 
-export async function getStack(request, reply) {
+export async function getStack(
+  request: FastifyRequest<{ Params: Params }>,
+  reply: FastifyReply
+): Promise<void> {
   const { id } = request.params;
   try {
     const stack = await prisma.stack.findUnique({
       where: { id: Number(id) },
     });
-    if (!stack) {
+    if (stack === null) {
       return reply.notFound('the specified id was not found');
     }
     return reply.status(200).send(stack);
   } catch (err) {
-    return reply.internalServerError(err);
+    return reply.internalServerError(`${err}`);
   }
+}
+
+interface Params {
+  id: string;
 }
